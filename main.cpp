af40a63c8e341d63751da0274f2b31004ba6f126
Yasin Baran Kaplan <101335414+YasinBaranKaplan@users.noreply.github.com>
2023-04-20 16:44:59 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,39 +4,36 @@ using namespace std;
 
 //"D" is our given array from users and "lenght" is our array lenght.
 
-void insertSort(int D[],int lenght){
+void printArrToConsole(int D[])
+{
+    for(int i=0;i<7;i++)
+    {
+        cout<<D[i]<<"\t";
+    }
+}
 
+void insertSort(int D[],int lenght)
+{
     int add;
 
-    for(int i=1;i<lenght;i++){
+    for(int i=1;i<lenght;i++)
+    {
         add=D[i];
-    int j= i-1;
+        int j= i-1;
 
-    while(j>=0 && D[j]>add){
-        D[j+1]=D[j];
-        j=j-1;
+        while(j>=0 && D[j]>add)
+        {
+            D[j+1]=D[j];
+            j=j-1;
+        }
+        D[j+1]=add;
     }
-    D[j+1]=add;
+    printArrToConsole(D);
 }
-}
 
-
-
-
-
-
-
-int main(){
+int main()
+{
     int tryArray[7]={2,7,15,4,3,100,-5};
 
     insertSort(tryArray,7);
-    for(int i=0;i<7;i++){
-        cout<<tryArray[i]<<"\t";
-    }
-
-
-
-
-
-
 }
